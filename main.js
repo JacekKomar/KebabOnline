42f5f51d7892f2e6903b8a8df9299b7c898910c8
JacekKomar <jacek.komar2000@gmail.com>
2022-09-06 10:45:23 +0200

    before trying to create working buttons

--- main.js
+++ main.js
@@ -5,14 +5,18 @@ console.log(kebabList);
 // kebabPreviev
 // kebabPreviev.kebabTitke
 const generateKebabOne = (kebabOne) => {
-  return `   <div class="Kebab-pizza" id="kebab-pizza">
+  return `   <div class= "Kebab-pizza" id="kebab-pizza">
 <img src="${kebabOne.img}" alt="Kebabbig" width="310px" height="210px" />
 <div class="titke">${kebabOne.titke}</div>
 <div class="price">${kebabOne.price}</div>
 <p class="desc">
   ${kebabOne.desc}
   </p>
-
+  <div class="plus-minus-buttons">
+  <i onclick="decrement" class="bi bi-dash-square"></i>
+  <div  class="quantity">0</div>
+  <i onclick="increment" class="bi bi-plus-square"></i>
+</div>
 <button class="button-kebab" data-order="kebabbig">Zamów</button>
 </div>`;
 };
@@ -20,5 +24,6 @@ const generateKebabOne = (kebabOne) => {
 //shop.innerHTML = generateKebabOne(kebabList[1]);
 shop.innerHTML += kebabList.map(generateKebabOne).join("");
 
-//czystki niepotrzebnych rzeczy np jacko show 1 , showJackoJeden, consol log wszystkie wywaic,
-// sprawdzic database - tam jest komentarz co zrobic
+// nie da się dodac id do powyzej
+
+let increment = () => {};
