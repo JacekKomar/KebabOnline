9140c1de924cf169e25a5a2b05d4c5db5d41eeec
JacekKomar <jacek.komar2000@gmail.com>
2022-09-14 18:35:02 +0200

    Created databaseBeer.js site

---
+++ databaseBeer.js
@@ -0,0 +1,91 @@
+const beerList = [
+  {
+    id: "beer1",
+    img: "imagies/drinks/beer_1.jpg",
+    titke: "Szlacheckie na miodzie",
+    price: 10.99,
+    desc: `Pojemność - 500 ml <br>
+      Zawartość alkoholu - 6,5% <br>
+      BLG - 15`,
+  },
+
+  {
+    id: "beer2",
+    img: "imagies/drinks/beer_2.jpg",
+    titke: "Polska wolność",
+    price: 12.5,
+    desc: `Pojemność - 500 ml <br>
+  Zawartość alkoholu 7% <br>
+  BLG - 18`,
+  },
+
+  {
+    id: "beer7",
+    img: "imagies/drinks/beer_3.jpg",
+    titke: "Piwo wolnościowe",
+    price: 9.99,
+    desc: `Pojemność - 500 ml <br>
+    Zawartość alkoholu - 4% <br>
+    BLG - 12`,
+  },
+
+  {
+    id: "beer4",
+    img: "imagies/drinks/beer_4.png",
+    titke: "Czarny łabędź",
+    price: 11.5,
+    desc: `Pojemność - 500 ml <br>
+    Zawartość alkoholu - 4% <br>
+    BLG - 11`,
+  },
+
+  {
+    id: "beer5",
+    img: "imagies/drinks/beer_5.jpg",
+    titke: "Filozofia XD",
+    price: 9.99,
+    desc: `Pojemność - 500 ml <br>
+    Zawartość alkoholu - 5% <br>
+    BLG - 13`,
+  },
+
+  {
+    id: "beer7",
+    img: "imagies/drinks/beer_7.png",
+    titke: "Strażnik teksasu",
+    price: 9.99,
+    desc: `Pojemność - 500 ml <br>
+    Zawartość alkoholu - 5% <br>
+    BLG - 12`,
+  },
+
+  {
+    id: "beer9",
+    img: "imagies/drinks/beer_9.png",
+    titke: "Paszport Mentzena",
+    price: 12.5,
+    desc: `Pojemność - 500 ml <br>
+    Zawartość alkoholu - 6% <br>
+    BLG - 15`,
+  },
+
+  {
+    id: "beer44",
+    img: "imagies/drinks/beer_44.jpg",
+    titke: "Inflacja",
+    price: 13.99,
+    desc: `Pojemność - 500 ml <br>
+    Zawartość alkoholu - 7,5% <br>
+    BLG - 18`,
+  },
+
+  {
+    id: "water",
+    img: "imagies/drinks/water.jpg",
+    titke: "Woda Franciszek",
+    price: 2,
+    desc: `Pojemność - 500 ml <br>
+    Gazowana <br>
+    Zdrowotna`,
+  },
+];
